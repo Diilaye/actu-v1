7d4c010d9b2948ee3e22a28ad23a8d5d9e97301b
Issa laye KANE <issa.k@deally.fr>
2024-08-10 23:25:40 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+
+
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const cors = require('cors');
+
+const path = require('path');
+
+const app = express();
+
+
+
+app.use(cors());
+
+app.use(bodyParser.json({
+    limit: '10000mb'
+}));
+
+app.use(bodyParser.urlencoded({
+    extended: true,
+    limit: '10000mb'
+}));
+
+app.use(express.urlencoded({ extended: true }));
+
+
+
+
+app.use(express.static(__dirname + '/web/'));
+
+
+
+app.get('*', (req, res) => res.sendFile(path.resolve('web/index.html')));
+
+
+
+
+app.listen(6900, () => {
+    console.log(`Server started on 6900`);
+});
