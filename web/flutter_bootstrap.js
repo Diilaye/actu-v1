0ef1dfed44658ea2467d1163e834e681701f940a
Issa laye KANE <issa.k@deally.fr>
2024-08-11 02:42:10 +0000

    first commit

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1445268175"
+    serviceWorkerVersion: "958390903"
   }
 });
