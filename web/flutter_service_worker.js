bf1bc441b6785a5f88a46e0315be83c8448c1a1e
Issa laye KANE <issa.k@deally.fr>
2025-03-06 16:11:11 +0000

    add news version

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "cd1b0b02fa9f648835772d12911aef01",
+const RESOURCES = {"flutter_bootstrap.js": "ff9fe5bade36cc4949815568fcb3df9b",
 "version.json": "48fb4be6ff8844e3de329e4141277ae0",
 "favicon.ico": "29715dde940081a24aad702b2a9823f3",
-"index.html": "817231e019afe0ad1fee3bb13b8f2e46",
-"/": "817231e019afe0ad1fee3bb13b8f2e46",
-"main.dart.js": "7310d05c507170b9bacf1d4b7144efe5",
+"index.html": "1748a0fad106b2844cde5c2594252b16",
+"/": "1748a0fad106b2844cde5c2594252b16",
+"main.dart.js": "3665378d8bd26cd397768086c3719996",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
 "favicon.png": "da6c91d80ed66da1d985b3fe479c8952",
 "icons/Icon-192.png": "da6c91d80ed66da1d985b3fe479c8952",
